45bd612fde059810856b2d50e06eba5184d05a5e
Evanf15 <evan.fretheim@ndus.edu>
2026-03-25 11:52:57 -0500

    Added button funcitonality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
+import { RouterLink } from '@angular/router';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        component: PostListComponent,
+        title: 'List of Posts'
+    },
+    {
+        path: 'post/:id',
+        component: PostDetailComponent,
+        title: 'Post Details'
+    }
 ];
 
 
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-
+  dataService = inject(DataService);
 }
